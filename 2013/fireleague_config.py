c07b2612c9f261bf01ad2c17f3edc1c6fa332520
ochawkeye <benjamin.j.schroeder@gmail.com>
2014-07-19 23:33:07 -0500

    How about a config file without my personal login info, huh?

--- 2013/fireleague_config.py
+++ 2013/fireleague_config.py
@@ -1,6 +1,7 @@
-username = 'benschroeder'
-password = 'toaben'
+username = 'XXXXXXXXX'
+password = 'XXXXXX'
 
+# Any league teams should be listed here:
 teams = [10106223, 10109051, 10110788, 10111577, 10112483, 10127676, 10141117,
          10166054, 10187930, 10199955, 10235905, 10282038, 10284052, 10340894,
          10347390, 10403070, 10481639, 10533123, 10684899, 10870530]
