724af8afd9d5fdff88b53bc06c2f0ee559d5323d
ochawkeye <benjamin.j.schroeder@gmail.com>
2017-01-17 11:02:37 -0600

    pretify balance table

--- playoff_winnings.py
+++ playoff_winnings.py
@@ -14,7 +14,7 @@ def current_winnings_from_db(table):
     query = "SELECT * FROM {}".format(table)
     cursor.execute(query)
 
-    balances = {row[1]: row[4] for row in cursor}
+    balances = {row[1]: [row[4], row[7]] for row in cursor}
 
     conn.close()
     return balances
@@ -22,13 +22,11 @@ def current_winnings_from_db(table):
 
 def update_players_winnings(table, player, add_amount):
     """Increase a single player's balance by the desired amount."""
-
     conn = pymssql.connect(db_config.SERVER, db_config.USER, db_config.PASSWORD)
     cursor = conn.cursor()
 
     cursor.execute("SELECT * FROM {} WHERE Players='{}'".format(table, player))
     current_balance = cursor.fetchone()[4]
-    print current_balance
     new_balance = current_balance + add_amount
 
     query = "UPDATE {} SET balance={} WHERE Players='{}'".format(table, new_balance, player)
@@ -38,14 +36,17 @@ def update_players_winnings(table, player, add_amount):
 
     conn.close()
 
-    return
+
+def print_winnings(balance_table):
+    for k, v in sorted(balance_table.items(), key=lambda e: e[0]):
+        if not v[1] and 'Select Your Name' not in k:
+            print '{:<40}\t{:>7}'.format(k, v[0])
 
 
 if __name__ == '__main__':
-    current_balance = current_winnings_from_db('Players')
-    print current_balance
+    # print_winnings(current_winnings_from_db('Players'))
 
     update_players_winnings('Players', 'Logan, Nicole', 0)
 
-    current_balance = current_winnings_from_db('Players')
+    print_winnings(current_winnings_from_db('Players'))
 
